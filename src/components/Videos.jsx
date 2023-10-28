bbfa5882bfd9248fcf58eaab53760f2834044d38
heejin <dlwlrgkwk82@gmail.com>
2023-10-28 16:23:23 +0900

    feat: add useQuery sample

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function Videos() {
   const { keyword } = useParams();
+
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ["videos", keyword],
+    queryFn: () =>
+      axios
+        .get(`/videos/${keyword ? "search" : "popular"}.json`)
+        .then((res) => res.data.items),
+  });
+
   return <div>videos {keyword ? keyword : "🔥"}</div>;
 }
