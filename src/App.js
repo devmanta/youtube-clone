3de5c0b6b885df818706fac04a1bd1ecc1ad8705
heejin <dlwlrgkwk82@gmail.com>
2023-10-28 15:59:11 +0900

    feat: add react query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
